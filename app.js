ebce5730624c72b956613ae8cb7f938855234f8d
Dennis Fernandes <dennisfernandes0212@gmail.com>
2023-07-08 21:23:46 +0700

    init

---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const morgan =  require('morgan');
+const app = express();
+const port = 3000
+
+// pake ejs
+app.set('view engine', 'ejs');
+
+// Third-party middleware
+app.use(expressLayouts);
+app.use(morgan('dev'));
+
+// build-in middleware
+app.use(express.static('public'));
+
+// Application Level Middleware
+app.use((req, res, next) => {
+  console.log('Time: ', Date.now());
+  next();
+});
+
+app.get('/', (req, res) => {
+  const member = [
+    {
+      nama : 'Bruce',
+      email : 'bruce@gmail.com'
+    },
+    {
+      nama : 'Dick',
+      email : 'dick@gmail.com'
+    },
+    {
+      nama : 'Damian',
+      email : 'damian@gmail.com'
+    },
+  ];
+
+  res.render('index', {
+    nama : 'John Doe',
+    title : 'Halaman Utama',
+    member : member,
+    layout : 'layouts/main-layout.ejs'});
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {title : 'Halaman About', layout : 'layouts/main-layout.ejs'});
+})
+
+app.get('/contact', (req, res) => {
+  res.render('contact', {title : 'Halaman Contact', layout : 'layouts/main-layout.ejs'});
+})
+
+app.get('/product/:id', (req, res) => {
+  res.send(`Product ID : ${req.params.id} <br>Category : ${req.query.category}`)
+})
+
+// kalo halaman ga ada
+app.use('/', (req, res) => {
+  res.status(404)
+  res.send(`<h1>Error : 404 Halaman Tidak Ada.</h1>`)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
