00120eb682cc5818de02b8906c21651e2783aa69
Dennis Fernandes <dennisfernandes0212@gmail.com>
2023-07-09 12:39:42 +0700

    menambahkan fungsi addContact

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
-const {loadContacts, findContact} = require('./utilities/contacts')
+const {loadContacts, findContact, addContact} = require('./utilities/contacts')
 
 const app = express();
 const port = 3000
@@ -13,8 +13,9 @@ app.use(expressLayouts);
 
 // build-in middleware
 app.use(express.static('public'));
+app.use(express.urlencoded());
 
-
+// Route root / halaman Home
 app.get('/', (req, res) => {
   const member = [
     {
@@ -36,30 +37,49 @@ app.get('/', (req, res) => {
     title : 'Halaman Utama',
     member : member,
     layout : 'layouts/main-layout.ejs'});
-})
+});
 
+// Route Halaman About
 app.get('/about', (req, res) => {
   res.render('about', {title : 'Halaman About', layout : 'layouts/main-layout.ejs'});
-})
+});
 
+// Route Halaman Contact
 app.get('/contact', (req, res) => {
   const contacts = loadContacts();
 
   res.render('contact', {title : 'Halaman Contact', layout : 'layouts/main-layout.ejs', contacts});
-})
+});
 
+// Router tambah contact
+// Harus ditulis sebelum route detail biar kebaca
+app.get('/contact/add', (req, res) => {
+  res.render('add-contact', {title : 'Form Tambah Contact', layout : 'layouts/main-layout.ejs'});
+});
+
+
+// Proses Tambah data contact
+app.post('/contact', (req, res) => {
+  addContact(req.body);
+
+  // kembali ke route get contact
+  res.redirect('/contact');
+});
+
+// Route Detail Contact
 app.get('/contact/:nama', (req, res) => {
   const contact = findContact(req.params.nama);
 
   res.render('detail', {title : 'Halaman Detail Contact', layout : 'layouts/main-layout.ejs', contact});
-})
+});
 
 
-// kalo halaman ga ada
+// Route kalo halaman ga ada
 app.use('/', (req, res) => {
   res.status(404)
   res.send(`<h1>Error : 404 Halaman Tidak Ada.</h1>`)
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- utilities/contacts.js
+++ utilities/contacts.js
@@ -7,14 +7,14 @@ if (!fs.existsSync(dirPath)) {
 }
 
 // check dan membuat file contact.json
-const filePath = "./data/contact.json";
+const filePath = "./data/contacts.json";
 if (!fs.existsSync(filePath)) {
   fs.writeFileSync(filePath, "[]", "utf-8");
 }
 
 // ambil data dari file JSON
 const loadContacts = () => {
-  const fileBuffer = fs.readFileSync("./data/contact.json", "utf-8");
+  const fileBuffer = fs.readFileSync("./data/contacts.json", "utf-8");
   const contacts = JSON.parse(fileBuffer);
   return contacts;
 };
@@ -26,4 +26,14 @@ const findContact = (nama) => {
   return contact
 };
 
-module.exports = { loadContacts, findContact };
+// menimpa file contacts.json dengan data baru
+const saveContacts = contacts => fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
+
+// menambahkan data contact
+const addContact = contact => {
+  const contacts = loadContacts();
+  contacts.push(contact);
+  saveContacts(contacts);
+};
+
+module.exports = { loadContacts, findContact, addContact };
